b3f67720f5d6767e0074c0c09e13756abb5e2a5a
CodySchaaf <CodySchaaf@users.noreply.github.com>
2024-01-09 18:25:18 -0700

    use-callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import { useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 import { MyExpensiveComponent } from "./MyExpensiveComponent.tsx";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  const config = {
-    appTitle: "foo",
-    appColor: "red",
+  const callback = () => {
+    console.log("veryBadUseCallback", count);
   };
 
-  const badMemoConfig = useMemo(() => ({
-    appTitle: "foo",
-    appColor: "red",
-  }));
+  const badUseCallback = useCallback(() => {
+    console.log("veryBadUseCallback", count);
+  });
 
-  const memoConfig = useMemo(
-    () => ({
-      appTitle: "foo",
-      appColor: "red",
-    }),
-    [],
-  );
+  const veryBadUseCallback = useCallback(() => {
+    console.log("veryBadUseCallback", count);
+  }, []);
+
+  const goodUseCallback = useCallback(() => {
+    console.log("veryBadUseCallback", count);
+  }, [count]);
 
   return (
     <>
+      Count1: {count}
+      <br />
       <button onClick={() => setCount(count + 1)}>Increment Count</button>
-      <MyExpensiveComponent config={config} />
-      <MyExpensiveComponent config={badMemoConfig} />
-      <MyExpensiveComponent config={memoConfig} />
+      <br />
+      Count2: {count2}
+      <br />
+      <button onClick={() => setCount2(count2 + 1)}>Increment Count</button>
+      <MyExpensiveComponent onClick={callback} />
+      <MyExpensiveComponent onClick={badUseCallback} />
+      <MyExpensiveComponent onClick={veryBadUseCallback} />
+      <MyExpensiveComponent onClick={goodUseCallback} />
     </>
   );
 }
