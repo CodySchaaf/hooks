0f58e90fff61e2b2ea1e773a76fea642a0a2d847
CodySchaaf <CodySchaaf@users.noreply.github.com>
2024-01-09 18:25:18 -0700

    use-callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,48 @@
-import { useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 import { MyExpensiveComponent } from "./MyExpensiveComponent.tsx";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  const config = {
-    appTitle: "foo",
-    appColor: "red",
+  // Tradition callback
+  const callback = () => {
+    console.log("veryBadUseCallback", count);
   };
 
-  const badMemoConfig = useMemo(() => ({
-    appTitle: "foo",
-    appColor: "red",
-  }));
+  // Not doing anything
+  const badUseCallback = useCallback(() => {
+    console.log("veryBadUseCallback", count);
+  });
 
-  const memoConfig = useMemo(
-    () => ({
-      appTitle: "foo",
-      appColor: "red",
-    }),
-    [],
-  );
+  // Missing its dependency array
+  const veryBadUseCallback = useCallback(() => {
+    console.log("veryBadUseCallback", count);
+  }, []);
 
+  const goodUseCallback = useCallback(() => {
+    console.log("veryBadUseCallback", count);
+  }, [count]);
+
+  // Note MyExpensiveComponent utilizes React.memo
   return (
     <>
+      <h5>Dependant Variable</h5>
+      Count1: {count}
+      <br />
       <button onClick={() => setCount(count + 1)}>Increment Count</button>
-      <MyExpensiveComponent config={config} />
-      <MyExpensiveComponent config={badMemoConfig} />
-      <MyExpensiveComponent config={memoConfig} />
+      <h5>Non-dependant Variable</h5>
+      Count2: {count2}
+      <br />
+      <button onClick={() => setCount2(count2 + 1)}>Increment Count</button>
+      <h5>Tradition callback</h5>
+      <MyExpensiveComponent onClick={callback} />
+      <h5>UseCallback not doing anything</h5>
+      <MyExpensiveComponent onClick={badUseCallback} />
+      <h5>Missing its dependency array</h5>
+      <MyExpensiveComponent onClick={veryBadUseCallback} />
+      <h5>Good!!</h5>
+      <MyExpensiveComponent onClick={goodUseCallback} />
     </>
   );
 }
--- src/MyExpensiveComponent.tsx
+++ src/MyExpensiveComponent.tsx
@@ -1,19 +1,16 @@
-import {memo, useRef} from "react";
+import { memo, useRef } from "react";
 
 interface Props {
-    config: {
-        appTitle: string;
-        appColor: string;
-    }
+  onClick: () => void;
 }
 
-export const MyExpensiveComponent = memo(({config}: Props) => {
-    const ref = useRef(0)
-    ref.current++
-    return (
-        <div>
-        <div>{ref.current}</div>
-        <div>{JSON.stringify(config)}</div>
-        </div>
-    )
-})
+export const MyExpensiveComponent = memo(({ onClick }: Props) => {
+  const ref = useRef(0);
+  ref.current++;
+  return (
+    <div>
+      <div>Renders: {ref.current}</div>
+      <button onClick={onClick}>Button</button>
+    </div>
+  );
+});
